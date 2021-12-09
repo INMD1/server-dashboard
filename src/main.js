a643822a044463542d71c8e6dbb741ac626d6109
INMD1 <87979171+INMD1@users.noreply.github.com>
2021-12-09 15:14:02 +0900

    update

--- src/main.js
+++ src/main.js
@@ -1,13 +1,14 @@
 import { createApp } from "vue";
 import router from "./router";
 import App from "./App.vue";
-
+import VuetifyDialog from 'vuetify-dialog'
 
 import "../src/assets/sidbars.css"
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
 
 
+Vue.use(VuetifyDialog)
 const app = createApp(App);
 app.use(router);
 
--- src/view/index.vue
+++ src/view/index.vue
@@ -9,20 +9,25 @@
     <div class="col-5">
       <div class="p-3">
         <h1 style="text-align: left">RAM</h1>
-        <ramstatus :Ramchart="Ramchart"  ref="ramstatus" />
+        <ramstatus :Ramchart="Ramchart" ref="ramstatus" />
         <br />
       </div>
     </div>
   </div>
   <div class="row">
-        <networkstatus :networkin="networkin" :networkout="networkout" ref="networkstatus"/>
+    <networkstatus
+      :networkin="networkin"
+      :networkout="networkout"
+      ref="networkstatus"
+    />
   </div>
 </template>
 
 <script>
 import cpustatus from "./main_chart/CpuD.vue";
 import ramstatus from "./main_chart/Ramchart.vue";
-import networkstatus from "./main_chart/networkchart.vue"
+import networkstatus from "./main_chart/networkchart.vue";
+
 let count = 0;
 import axios from "axios";
 export default {
@@ -30,53 +35,55 @@ export default {
   components: {
     cpustatus,
     ramstatus,
-    networkstatus
+    networkstatus,
   },
   data() {
     return {
       cpudata: [],
       Ramchart: [],
-      networkin: [0,0,0,0,0,0],
-      networkout: [0,0,0,0,0,0],
+      networkin: [0, 0, 0, 0, 0, 0],
+      networkout: [0, 0, 0, 0, 0, 0],
       chartOptions: [],
     };
   },
   async mounted() {
-    this.timer = 50
+    this.timer = 50;
     setInterval(async () => {
-      const getdata = await axios.get("http://113.198.229.165:9090/test");
-      let num = getdata.data.cpu.usage;
-      //cpudata_전처리
-      this.cpudata[0] = num;
-      this.cpudata[1] = 100 - num;     
+      try {
+        const getdata = await axios.get("http://113.198.229.165:9090/test");
+        let num = getdata.data.cpu.usage;
+        //cpudata_전처리
+        this.cpudata[0] = num;
+        this.cpudata[1] = 100 - num;
 
-      // Ramdata_전처리
-      let totalMemMb = getdata.data.ram.totalMemMb;
-      let usedMemMb = getdata.data.ram.usedMemMb;
-      const persent = (usedMemMb / totalMemMb) * 100;
-      
-      this.Ramchart[0] = persent;
-      this.Ramchart[1] = (100 - persent)
+        // Ramdata_전처리
+        let totalMemMb = getdata.data.ram.totalMemMb;
+        let usedMemMb = getdata.data.ram.usedMemMb;
+        const persent = (usedMemMb / totalMemMb) * 100;
 
+        this.Ramchart[0] = persent;
+        this.Ramchart[1] = 100 - persent;
 
-    // network 전처리 메인페이지는 토탈로 표시됨
-    let inputMb = getdata.data.netstats.total.inputMb;
-    let outputMb = getdata.data.netstats.total.outputMb
-    if(!(count > 6)){
-      this.networkin[count] = inputMb;
-      this.networkout[count] = outputMb;
-      count ++;
-    }else if(count > 6){
-      count = 0;
-      this.networkin[count] = inputMb;
-      this.networkout[count] = outputMb;
-      count ++;
-    }
-    this.$refs.cpustatus.outchange();
-    this.$refs.networkstatus.outchange();
-    this.$refs.ramstatus.outchange();
+        // network 전처리 메인페이지는 토탈로 표시됨
+        let inputMb = getdata.data.netstats.total.inputMb;
+        let outputMb = getdata.data.netstats.total.outputMb;
+        if (!(count > 6)) {
+          this.networkin[count] = inputMb;
+          this.networkout[count] = outputMb;
+          count++;
+        } else if (count > 6) {
+          count = 0;
+          this.networkin[count] = inputMb;
+          this.networkout[count] = outputMb;
+          count++;
+        }
+        this.$refs.cpustatus.outchange();
+        this.$refs.networkstatus.outchange();
+        this.$refs.ramstatus.outchange();
+      } catch (error) {
 
-   },5000);
+      }
+    }, 5000);
   },
 };
 </script>
